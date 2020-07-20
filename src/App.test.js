535b942275bd49e027841cd3846111f9d4363cfc
Kirill Zhdanov <storenth@yandex.ru>
2020-07-20 13:50:12 +0700

    Create Add item userEvent integration test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
+// import './mock/matchMedia.mock'; // Must be imported before the tested file
 import React from 'react';
-import {render} from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import {App} from './App';
 
+function renderApp() {
+  return render(<App />);
+}
+
 test(`renders Todo's`, () => {
-  const {getByText} = render(<App />);
-  const linkElement = getByText(/Todo's/i);
+  const {getByText} = renderApp();
+  const linkElement = getByText(/No todos left/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('add item', async () => {
+  renderApp();
+  const inputNewTodo = screen.getByLabelText('Add new todo:');
+  userEvent.type(inputNewTodo, 'reactjs');
+  const submitButton = screen.getByRole('button', {name: /Add item/i});
+  userEvent.click(submitButton);
+  const addedTodo = await screen.findByText(/reactjs/i);
+  screen.debug(addedTodo);
+  expect(addedTodo).toBeInTheDocument();
+  screen.debug();
 });
